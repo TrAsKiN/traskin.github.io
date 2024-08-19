e92c861406717620833201bbdba0933265f73cdf
TrAsKiN <marchal.simon@gmail.com>
2024-08-19 14:17:09 +0200

    chore: Update app component with standalone attribute and import RouterModule

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'tg-root',
+  standalone: true,
   templateUrl: './app.component.html',
+  imports: [RouterModule],
 })
-export class AppComponent
-{
+export class AppComponent {
   constructor(title: Title) {
     title.setTitle('MARCHAL "TrAsKiN" Simon · Développeur web PHP/Symfony');
   }
